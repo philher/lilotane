571fa50ac725334568c3d898e179cce80948b9b3
Philippe <31505982+philher@users.noreply.github.com>
2024-04-22 18:32:27 +0200

    filenames

--- lib/glucose4/fetch_and_build.sh
+++ lib/glucose4/fetch_and_build.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d glucose-4 ]; then
     wget https://github.com/audemard/glucose/archive/refs/tags/4.1.tar.gz
-    tar xzvf glucose-4.1.tgz
+    tar xzvf 4.1.tar.gz
     mv glucose-4.1 glucose-4
     patch glucose-4/core/Solver.h < Solver.h.patch
     patch glucose-4/core/Solver.cc < Solver.cc.patch
